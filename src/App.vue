3cc3db3fc895fde27c3fcb306fdc8244592ef110
WolbachAuth0 <aaron.wolbach@auth0.com>
2025-08-05 09:51:04 -0400

    update html

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="app" class="d-flex flex-column h-100">
-    <nav-bar />
+  <div id="app" class="d-flex flex-column">
+    <Navigation />
     <div class="container">
       <error />
-      <BCard border-variant="none" class="mt-5">
+      <BCard border-variant="primary" variant="secondary" class="mt-2">
       <router-view />
     </BCard>
 
@@ -12,13 +12,13 @@
   </div>
 </template>
 
-<script lang="ts">
-import NavBar from "./components/NavBar.vue";
+<script>
+import Navigation from "./components/Navigation.vue";
 import Error from "./components/Error.vue";
 
 export default {
   components: {
-    NavBar,
+    Navigation,
     Error
   }
 };
